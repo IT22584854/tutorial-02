6eee7cd093061bca8b2e62e82b29414e7973f9da
IT22584854-Shehan <122781054+IT22584854@users.noreply.github.com>
2023-03-21 02:17:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@tutorial-02/excersice01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>>
+
+using namespace std;
+
+int main()
+{
+  float cm,inches;
+
+  cout<<"enter a length in cm : "<<endl;
+  cin>>cm;
+
+  inches = cm / 2.54;
+
+  cout<<"Legth in inches is "<< inches<<endl;
+  return 0;
+}
